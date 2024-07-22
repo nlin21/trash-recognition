a0329f8ef0db5dbe33af172b253b569d6b30e9ce
Nicky Lin <nickylin.328@gmail.com>
2024-07-22 11:44:28 -0400

    Rename

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from ultralytics import YOLO
+# Starter code
+
 import torch
+from ultralytics import YOLO
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 if device == "0":
@@ -7,5 +9,5 @@ if device == "0":
 
 print(f'Using device: {device}')
 
-model = YOLO("yolov8m.pt").to(device)
+model = YOLO("yolov8n.pt").to(device)
 results = model.track(source=0, show=True, save=True)
