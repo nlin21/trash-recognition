02c50b7ffa4fd43dcd0eaf18f11743a6165fa355
Nicky Lin <nickylin.328@gmail.com>
2024-08-01 11:55:48 -0400

    Document

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# Starter code
+# For use with HTTP stream from ESP32-WROVER camera module
+# 
+# TODO: Look into converting to RTSP stream for direct integration with ultralyitcs.
+# Right now, cv2 is used to capture and infer on each frame
 
 import torch
 from ultralytics import YOLO
 import cv2
-import time
-import sys
 
 if __name__ == "__main__":
     url = sys.argv[1] if len (sys.argv) > 1 else 'http://129.161.161.235/stream'
